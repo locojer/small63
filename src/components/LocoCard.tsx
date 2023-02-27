5a397a3bb246278483b8aad8e7be756bb390de7d
locojer <jeraldine+test1708@locofy.ai>
2023-02-27 17:04:50 +0000

    Locofy commit

---
+++ src/components/LocoCard.tsx
@@ -0,0 +1,73 @@
+import { FunctionComponent, useMemo } from "react";
+import CSS, { Property } from "csstype";
+import styles from "./LocoCard.module.css";
+
+type LocoCardType = {
+  destinationImageUrl?: string;
+  destinationName?: string;
+  destinationPrice?: string;
+
+  /** Style props */
+  propWidth?: Property.Width;
+  propDisplay?: Property.Display;
+  propWidth1?: Property.Width;
+  propLeft?: Property.Left;
+
+  /** Action props */
+  onCol3Click?: () => void;
+};
+
+const LocoCard: FunctionComponent<LocoCardType> = ({
+  destinationImageUrl,
+  destinationName,
+  destinationPrice,
+  propWidth,
+  propDisplay,
+  propWidth1,
+  propLeft,
+  onCol3Click,
+}) => {
+  const detailsStyle: CSS.Properties = useMemo(() => {
+    return {
+      width: propWidth,
+    };
+  }, [propWidth]);
+
+  const divStyle: CSS.Properties = useMemo(() => {
+    return {
+      display: propDisplay,
+      width: propWidth1,
+    };
+  }, [propDisplay, propWidth1]);
+
+  const fromStyle: CSS.Properties = useMemo(() => {
+    return {
+      left: propLeft,
+    };
+  }, [propLeft]);
+
+  return (
+    <button className={styles.col1} onClick={onCol3Click}>
+      <div className={styles.pariscard}>
+        <img
+          className={styles.parisimageIcon}
+          alt=""
+          src={destinationImageUrl}
+        />
+        <div className={styles.destinationDetails}>
+          <b className={styles.paris}>{destinationName}</b>
+          <div className={styles.details} style={detailsStyle}>
+            <div className={styles.div} style={divStyle}>
+              {destinationPrice}
+            </div>
+            <div className={styles.from} style={fromStyle}>
+              from
+            </div>
+          </div>
+        </div>
+      </div>
+    </button>
+  );
+};
+
+export default LocoCard;
---
+++ src/pages/Small63.tsx
@@ -0,0 +1,73 @@
+import { FunctionComponent, useCallback } from "react";
+import LocoCard from "../components/LocoCard";
+import styles from "./Small63.module.css";
+
+const Small63: FunctionComponent = () => {
+  const onCol3Click = useCallback(() => {
+    // Please sync "Results Page" to the project
+  }, []);
+
+  const onCol4Click = useCallback(() => {
+    window.open("https://www.bing.com/");
+  }, []);
+
+  return (
+    <div className={styles.small63}>
+      <div className={styles.destinationsTitles}>
+        <div className={styles.titleContainer}>
+          <b className={styles.planYourNext}>Plan your next trip</b>
+          <b className={styles.mostPopularDestinations}>
+            Most Popular Destinations
+          </b>
+        </div>
+        <button className={styles.viewAllTop}>
+          <div className={styles.viewAllDestinations}>
+            View all destinations
+          </div>
+          <img className={styles.arrowRightIcon} alt="" src="/arrowright.svg" />
+        </button>
+      </div>
+      <div className={styles.cardsContainer}>
+        <LocoCard
+          destinationImageUrl="/parisimage@2x.png"
+          destinationName="Paris"
+          destinationPrice="$699"
+          propWidth="90.92px"
+          propDisplay="inline-block"
+          propWidth1="90.92px"
+          propLeft="38.39px"
+        />
+        <LocoCard
+          destinationImageUrl="/greeceimage@2x.png"
+          destinationName="Greece"
+          destinationPrice="$1079"
+          propWidth="99.92px"
+          propDisplay="unset"
+          propWidth1="unset"
+          propLeft="47.39px"
+        />
+        <LocoCard
+          destinationImageUrl="/norwayimage@2x.png"
+          destinationName="Norway"
+          destinationPrice="$895"
+          onCol3Click={onCol3Click}
+        />
+        <LocoCard
+          destinationImageUrl="/tuscanyimage@2x.png"
+          destinationName="Tuscany"
+          destinationPrice="$1245"
+          propWidth="100.62px"
+          propWidth1="100.62px"
+          propLeft="48.09px"
+          onCol3Click={onCol4Click}
+        />
+      </div>
+      <div className={styles.viewAllBottom}>
+        <div className={styles.viewAllDestinations1}>View all destinations</div>
+        <img className={styles.arrowRightIcon} alt="" src="/arrowright1.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default Small63;
